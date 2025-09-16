03e7c00df8d175b3a4cac6ec9c513102b6dd5298
Go Nishimoto <nishimoto.go@gmail.com>
2025-09-16 14:10:10 +0900

    add index.ts

---
+++ lambda/index.ts
@@ -0,0 +1,15 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Event: ', event);
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: 'Hello from Lambda!',
+    }),
+  };
+  return response;
+};
--- lib/simple-lambda-app-stack.ts
+++ lib/simple-lambda-app-stack.ts
@@ -1,31 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
-export class CdkTsLambdaStack extends cdk.Stack {
+export class SimpleLambdaAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // 1. Lambda関数の定義
-    // NodejsFunctionコンストラクトは、src/index.tsのコードを自動でビルド・バンドルする
-    const simpleLambda = new lambdaNodejs.NodejsFunction(this, 'SimpleLambdaFunction', {
-      entry: 'src/index.ts', // コードのエントリーポイントを指定
-      handler: 'handler', // ハンドラー関数の名前を指定
-      runtime: lambda.Runtime.NODEJS_20_X, // Node.jsの最新LTSバージョンを選択
+    const myLambda = new lambda.Function(this, 'MySimpleLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda'),
     });
 
-    // 2. Lambda Function URLの定義
-    // これにより、Lambda関数を直接呼び出すためのHTTPSエンドポイントが作成される
-    const lambdaFunctionUrl = simpleLambda.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE, // 認証なしでパブリックにアクセス可能に設定
+    // 2. API Gatewayの定義とLambdaとの連携
+    const api = new apigateway.RestApi(this, 'MySimpleApi', {
+      restApiName: 'My Simple API',
     });
 
-    // 3. デプロイ後のエンドポイントURLを出力
-    // これにより、cdk deploy完了後にURLがターミナルに表示される
-    new cdk.CfnOutput(this, 'LambdaFunctionUrl', {
-      value: lambdaFunctionUrl.url,
-      description: 'The URL for the simple Lambda function endpoint.',
-    });
+    const integration = new apigateway.LambdaIntegration(myLambda);
+    api.root.addMethod('GET', integration); // GETリクエストでLambdaを呼び出す
   }
 }
