fd8aa9381eaf27d96d363aa13601cc7b7712e3c5
Go Nishimoto <nishimoto.go@gmail.com>
2025-09-16 14:25:50 +0900

    add index.ts

--- lib/simple-lambda-app-stack.ts
+++ lib/simple-lambda-app-stack.ts
@@ -2,24 +2,25 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class SimpleLambdaAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // 1. Lambda関数の定義
-    const myLambda = new lambda.Function(this, 'MySimpleLambda', {
+    // NodejsFunctionを使ってLambda関数を定義
+    const myLambda = new NodejsFunction(this, 'MySimpleLambda', {
       runtime: lambda.Runtime.NODEJS_20_X,
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset('lambda'),
+      entry: 'lambda/index.ts', // <-- TypeScriptのソースコードへのパス
+      handler: 'handler', // <-- 実行する関数名
     });
 
-    // 2. API Gatewayの定義とLambdaとの連携
+    // API Gatewayの定義とLambdaとの連携
     const api = new apigateway.RestApi(this, 'MySimpleApi', {
       restApiName: 'My Simple API',
     });
 
     const integration = new apigateway.LambdaIntegration(myLambda);
-    api.root.addMethod('GET', integration); // GETリクエストでLambdaを呼び出す
+    api.root.addMethod('GET', integration);
   }
 }
